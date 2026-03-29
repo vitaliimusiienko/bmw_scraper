9920333cca1ed9a79870b5e613808b4de15b685b
Vitalii Musiienko <vitaliimusiienko10@gmail.com>
2026-03-29 20:13:05 +0300

    feat: added RandomUserAgentMiddleware in middlewaares.py

--- scraper/scraper/middlewares.py
+++ scraper/scraper/middlewares.py
@@ -3,98 +3,119 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
+import random
+import logging
 from scrapy import signals
 
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+logger = logging.getLogger(__name__)
+ 
 
+# This is file where i added custom middlewares for my Scrapy project. 
+# It includes a middleware to randomize the User-Agent header for each request, 
+# as well as standard spider and downloader middlewares that can be customized further if needed. 
+# The RandomUserAgentMiddleware helps to mimic different browsers and reduce the chances of being blocked by the target website.
 
-class ScraperSpiderMiddleware:
-    # Not all methods need to be defined. If a method is not defined,
-    # scrapy acts as if the spider middleware does not modify the
-    # passed objects.
+class RandomUserAgentMiddleware:
 
+    USER_AGENTS = [
+        (
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/122.0.0.0 Safari/537.36"
+        ),
+        (
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) "
+            "Gecko/20100101 Firefox/124.0"
+        ),
+        (
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_4) "
+            "AppleWebKit/605.1.15 (KHTML, like Gecko) "
+            "Version/17.3.1 Safari/605.1.15"
+        ),
+        (
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/122.0.0.0 Safari/537.36"
+        ),
+        (
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0"
+        ),
+        (
+            "Mozilla/5.0 (X11; Linux x86_64) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/122.0.0.0 Safari/537.36"
+        ),
+        (
+            "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:124.0) "
+            "Gecko/20100101 Firefox/124.0"
+        ),
+    ]
     @classmethod
     def from_crawler(cls, crawler):
-        # This method is used by Scrapy to create your spiders.
+        instance = cls()
+        instance.crawler = crawler
+        return instance
+
+    def process_request(self, request):
+        user_agent = random.choice(self.USER_AGENTS)
+        request.headers["User-Agent"] = user_agent
+        logger.debug(
+            "RandomUserAgentMiddleware: выбран User-Agent -> %s | URL: %s",
+            user_agent,
+            request.url,
+        )
+ 
+
+# Standard Spider and Downloader middlewares (templates) that can be further customized if needed. 
+# They currently just pass through the data without modification, but they include hooks for processing spider output, 
+# handling exceptions, and logging when the spider is opened. 
+# These can be expanded to add additional functionality as required by the scraping project.
+
+class BmwScraperSpiderMiddleware:
+    @classmethod
+    def from_crawler(cls, crawler):
         s = cls()
         crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
         return s
-
-    def process_spider_input(self, response, spider):
-        # Called for each response that goes through the spider
-        # middleware and into the spider.
-
-        # Should return None or raise an exception.
-        return None
-
+ 
     def process_spider_output(self, response, result, spider):
-        # Called with the results returned from the Spider, after
-        # it has processed the response.
-
-        # Must return an iterable of Request, or item objects.
         for i in result:
             yield i
-
+ 
     def process_spider_exception(self, response, exception, spider):
-        # Called when a spider or process_spider_input() method
-        # (from other spider middleware) raises an exception.
-
-        # Should return either None or an iterable of Request or item objects.
         pass
-
-    async def process_start(self, start):
-        # Called with an async iterator over the spider start() method or the
-        # matching method of an earlier spider middleware.
-        async for item_or_request in start:
-            yield item_or_request
-
+ 
+    def process_start_requests(self, start_requests, spider):
+        for r in start_requests:
+            yield r
+ 
     def spider_opened(self, spider):
-        spider.logger.info("Spider opened: %s" % spider.name)
+        spider.logger.debug("Spider opened: %s" % spider.name)
+ 
 
+# Standard Downloader Middleware (template) that can be further customized if needed. 
+# It currently just passes through the requests and responses without modification, 
+# but it includes hooks for processing requests, handling exceptions, and logging when the spider is opened. 
+# This can be expanded to add additional functionality as required by the scraping project.
 
-class ScraperDownloaderMiddleware:
-    # Not all methods need to be defined. If a method is not defined,
-    # scrapy acts as if the downloader middleware does not modify the
-    # passed objects.
-
+class BmwScraperDownloaderMiddleware:
     @classmethod
     def from_crawler(cls, crawler):
-        # This method is used by Scrapy to create your spiders.
         s = cls()
         crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
         return s
-
-    def process_request(self, request, spider):
-        # Called for each request that goes through the downloader
-        # middleware.
-
-        # Must either:
-        # - return None: continue processing this request
-        # - or return a Response object
-        # - or return a Request object
-        # - or raise IgnoreRequest: process_exception() methods of
-        #   installed downloader middleware will be called
+ 
+    # ИСПРАВЛЕНИЕ: убран аргумент spider
+    def process_request(self, request):
         return None
-
+ 
     def process_response(self, request, response, spider):
-        # Called with the response returned from the downloader.
-
-        # Must either;
-        # - return a Response object
-        # - return a Request object
-        # - or raise IgnoreRequest
         return response
-
+ 
     def process_exception(self, request, exception, spider):
-        # Called when a download handler or a process_request()
-        # (from other downloader middleware) raises an exception.
-
-        # Must either:
-        # - return None: continue processing this exception
-        # - return a Response object: stops process_exception() chain
-        # - return a Request object: stops process_exception() chain
         pass
-
+ 
     def spider_opened(self, spider):
-        spider.logger.info("Spider opened: %s" % spider.name)
+        spider.logger.debug("Spider opened: %s" % spider.name)
